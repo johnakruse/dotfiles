cf946f0363fe23ce021499077b574ddf3249595c
John Kruse <johnalphonsekruse@gmail.com>
2017-11-03 23:20:32 -0400

    making things prettier

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.FadeInactive
+import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -9,13 +11,14 @@ import XMonad.Util.Run(spawnPipe)
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Layout.Spiral
 import XMonad.Layout.Accordion
+import XMonad.Layout.Grid
 import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| spiral (6/7) ||| Accordion
+myLayout = tiled ||| Mirror tiled ||| Full ||| spacedspiral ||| Accordion
     where
-        tiled = Tall nmaster delta ratio
+        tiled = spacing 3 $ Tall nmaster delta ratio
     
         nmaster = 1
     
@@ -23,28 +26,43 @@ myLayout = tiled ||| Mirror tiled ||| Full ||| spiral (6/7) ||| Accordion
     
         delta = 3/100
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","10"]
+        spacedspiral = spacing 3 $ spiral (6/7)
+
+myWorkspaces = ["一","二","三","四","五","六","七","八","九"]
 
 myManageHook = composeAll
     [ isFullscreen --> doFullFloat
-    , className =? "Pidgin" --> doShift "2"
     , className =? "Gimp"   --> doFloat
     ]
 
+wsPP =    def { ppOrder = \(ws:_:t:_) -> [ws]
+                , ppCurrent = xmobarColor "green" ""
+                , ppHidden = xmobarColor "white" ""
+                , ppUrgent = xmobarColor "red" "yellow"
+                }
+
+tilePP = def { ppOrder = \(ws:_:t:_) -> [t]
+             , ppTitle = xmobarColor "orange" "" . shorten 50
+             }
+
+myLogHook = fadeInactiveLogHook fadeAmount
+    where fadeAmount = 0.8
+
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/john/.xmobarrc"
+    curtile <- spawnPipe "cat > /tmp/.currenttile"
+    xmproc <- spawnPipe "/home/john/.cabal/bin/xmobar /home/john/.xmobarrc"
     xmonad $ ewmh defaultConfig
         { modMask = mod4Mask
         , workspaces = myWorkspaces
-        , manageHook = manageDocks <+> myManageHook <+>  manageHook defaultConfig
+        , borderWidth = 0
+        , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ smartBorders(myLayout)
         , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "orange" "" . shorten 50
-            }
+        , logHook = dynamicLogWithPP wsPP { ppOutput = hPutStrLn xmproc }
+                <+> dynamicLogWithPP tilePP { ppOutput = hPutStrLn curtile }
+                <+> myLogHook
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((shiftMask, xK_Print), spawn "scrot")
         , ((0, 0x1008ff13), spawn "amixer set Master 2+")
